26026d464a9b167d9310aeb05e179cbc06e40e09
Sujeet <sujeet.panmand@baxture.com>
2024-03-04 17:58:07 +0530

    fix issue of google form integration not working

--- js/contact-us.js
+++ js/contact-us.js
@@ -1,3 +1,59 @@
+const form = document.getElementById("contactUs");
+form.target = 'contactUsIframe';
+
+var loaderElement = document.getElementById("ajaxLoader");
+
+// detect when the iframe reloads
+var iframe = document.getElementById('contactUsIframe');
+if (iframe) {
+  iframe.onload = function () {
+    loaderElement.style.display = "none";
+    alert("Your enquiry submitted successfully");
+    form.reset();
+  }
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  var formData = new FormData(form);
+  // var nameRegex = new RegExp("^[a-zA-Zs]*$");
+  var emailRegex = new RegExp(/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/);
+  var phoneNumberRegex = new RegExp(/^\d+$/);
+  var emptyRegex = new RegExp(/^[^]+$/);
+
+  var name = formData.get("entry.1318155791");
+  var email = formData.get("entry.1905198810");
+  var phoneNumber = formData.get("entry.1578023363");
+  var message = formData.get("entry.1298951158");
+
+  // if (!emptyRegex.test(name) || !nameRegex.test(name)) {
+  if (!emptyRegex.test(name)) {  
+    alert("Please enter valid name");
+    return false;
+  }
+
+  if (!emailRegex.test(email)) {
+    alert("Please enter valid email");
+    return false;
+  }
+
+  if (!phoneNumberRegex.test(phoneNumber)) {
+    alert("Please enter valid phone number");
+    return false;
+  }
+
+  if (!emptyRegex.test(message)) {
+    alert("Message field should not empty");
+    return false;
+  }
+  
+  loaderElement.style.display = "block";
+
+  form.submit();
+
+});
+
 function submitEnquiry() {
   const form = document.getElementById("contactUs");
   var formData = new FormData(form);
